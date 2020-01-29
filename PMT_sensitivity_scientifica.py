118de0a5827b2a94509b734895bce761385a94b3
Kate Roll <kater@alleninstitute.org>
2020-01-29 11:54:18 -0800

    added new functions

--- PMT_sensitivity_scientifica.py
+++ PMT_sensitivity_scientifica.py
@@ -16,6 +16,7 @@ def get_raw_files(file_folder):
                               '*.raw')) # Wildcard.raw = any file ending in .raw
     return raw_files
 
+
 def get_ini_files(file_folder):
     ini_files = glob(os.path.join(file_folder, 
                               '*', # Wildcard = any sub-directory 
@@ -23,31 +24,71 @@ def get_ini_files(file_folder):
     return ini_files
 
 
+def get_filename(file_path):
+    filename = os.path.split(file_path)[-1]
+    return filename
+
+
+def get_file_timestamp(file_path):
+    filename = get_filename(file_path)
+    timestamp = filename[:17]
+    return timestamp
+
+
+def check_if_shuttered(file_path):
+    filename = get_filename(file_path)
+    shuttered = "shuttered" in filename
+    return shuttered
 
 def read_ini_file(ini_filepath):
     config.read(ini_file_path)
 
-def metadata_from_ini_file([list_of_keys]):
+
+def metadata_from_ini_file(list_of_keys):
     metadata_df = pd.DataFrame()
     for key in list_of_keys:
         data = config['_'][key]
-        metadata_df.loc[:,key] = data
+        metadata_df= metadata_df.loc[:,key] = data
 
 
-def get_filename_timestamp():
+def load_rawdata(raw_filepath):
+    data = np.fromfile(raw_filepath, dtype=">u2")
+    return data
 
-def get_raw_data(raw_filepath):
-    data = np.fromfile(os.path.join(dest,file), dtype=">u2")
-    raw_df = pd.DataFrame({})
+def gen_single_rawfile_df(raw_filepath):
+    data = load_rawdata(raw_filepath)
+    
+    timestamp = get_file_timestamp(raw_filepath)
+    shuttered = check_if_shuttered(raw_filepath)
+
     raw_data_mean = int(np.mean(data))
     raw_data_std_dev = int(np.std(data))
     raw_data_var = int(np.var(data))
+    
+    raw_df = pd.DataFrame({"timestamp":timestamp,
+                            "shuttered":shuttered,
+                            "frame_mean": raw_data_mean, 
+                            "frame_std": raw_data_std_dev, 
+                            "frame_var": raw_data_var}, 
+                            index =[0])
+    return raw_df
+
+def gen_multi_rawfile_df(list_of_filepaths):
+    multi_rawfile_df = pd.DataFrame()
+    for rawfile in list_of_filepaths:
+        single_df = gen_single_rawfile_df(rawfile)
+        multi_rawfile_df.append(single_df)
+    multi_rawfile_df = multi_rawfile_df.reset_index(drop=True)
+    return multi_rawfile_df
+
+
+def gen_raw_data_df()
+    
 
 
 
 def get_pmt_gain_offset(dataframe):
-    pmt_gain_df = dataframe.loc[dataframe["laser_power"]=="shuttered", ["frame_mean"]].copy
-
+    pmt_gain_df = dataframe.loc[dataframe["laser_power"]=="shuttered", ["pmt_gain","frame_mean"]].copy
 
 
 
